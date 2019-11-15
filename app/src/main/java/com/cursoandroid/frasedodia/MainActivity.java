158f443c74a14d7589180a6dd4caa06ecb4e15b4
Inhandui Barbosa Ferreira <inhandui@gmail.com>
2019-11-15 15:18:28 -0300

    Adding more motivational Phrases (By Gary Vee).

--- app/src/main/java/com/cursoandroid/frasedodia/MainActivity.java
+++ app/src/main/java/com/cursoandroid/frasedodia/MainActivity.java
@@ -14,9 +14,18 @@ public class MainActivity extends AppCompatActivity {
     private Button novaFrase; //Button to perform an action to show a new phrase
     private TextView frase; //actual phrase
 
-    private String[] frases = { "Please understand this ... It's important to wake up everyday and feel like it's Saturday - Gary Vee",
-            "You don't decide what's quality, the market does - Gary Vee",
-            "Give, give, give, then ask - Gary Vee"
+    private String[] frases = { "Please understand this ... It's important to wake up everyday and feel like it's Saturday. - Gary Vee",
+            "You don't decide what's quality, the market does. - Gary Vee",
+            "Give, give, give, then ask. - Gary Vee",
+            "Skills are cheap. Passion is priceless. - Gary Vee",
+            "No matter what you do, your job is to tell your story. - Gary Vee",
+            "Look yourself in the mirror and ask yourself, what do I want to do everyday for the rest of my life…do that. - Gary Vee",
+            "The truth is that finding happiness in what you do every day is so imperative. - Gary Vee",
+            "There is never a bad time to start a business – unless you want to start a mediocre one. - Gary Vee",
+            "All your ideas may be solid or even good .. But you have to Actually EXECUTE on them for them to matter. - Gary Vee",
+            "Love your family, work super hard, live your passion. - Gary Vee",
+            "Anyone giving you hate has it way worse than you. - Gary Vee",
+            "You can't read about push ups. You gotta do them. - Gary Vee"
     };
 
     private Random random;
