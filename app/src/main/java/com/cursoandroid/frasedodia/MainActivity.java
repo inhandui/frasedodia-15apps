9a1902250ed54d444c8c8b52e502d1e3e0b0e81d
Inhandui Barbosa Ferreira <inhandui@gmail.com>
2019-11-15 15:04:18 -0300

    Adding project attributes.

--- app/src/main/java/com/cursoandroid/frasedodia/MainActivity.java
+++ app/src/main/java/com/cursoandroid/frasedodia/MainActivity.java
@@ -3,12 +3,32 @@ package com.cursoandroid.frasedodia;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button novaFrase; //Button to perform an action to show a new phrase
+    private TextView frase; //actual phrase
+
+    private String[] frases = { "Please understand this ... It's important to wake up everyday and feel like it's Saturday - Gary Vee",
+            "You don't decide what's quality, the market does - Gary Vee",
+            "Give, give, give, then ask - Gary Vee"
+    };
+
+    private Random random;
+
+    private int numero;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
     }
 }
